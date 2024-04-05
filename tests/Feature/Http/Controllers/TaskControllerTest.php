83ed3a0f40788d4793065ce0a7a90daa64378c52
Meirbek <m.bolatbekov@andersenlab.com>
2024-04-05 12:58:59 +0500

    added funcitonal tests

--- tests/Feature/Http/Controllers/TaskControllerTest.php
+++ tests/Feature/Http/Controllers/TaskControllerTest.php
@@ -2,25 +2,88 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class TaskControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    private const API_ROUTE = '/api/tasks/';
+
+    public function test_tasks_index_unauthorized(): void
     {
-        /*
+        // Act
+        $response = $this->get(self::API_ROUTE);
 
-            404. Ups, tests not found, come later ;)
-
-        */
-
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        // Assert
+        $response->assertJsonFragment(['success' => false]);
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
     }
+
+    public function test_tasks_index_authorized(): void
+    {
+        // Arrange
+        Sanctum::actingAs(User::factory()->create());
+
+        // Act
+        $response = $this->get(self::API_ROUTE);
+
+        // Assert
+        $response->assertJsonFragment(['success' => true]);
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    public function test_tasks_store_unauthorized(): void
+    {
+        // Arrange
+        $task = [
+            'name' => 'Test task',
+            'description' => 'Test description'
+        ];
+
+        // Act
+        $response = $this->post(self::API_ROUTE, $task);
+
+        // Assert
+        $response->assertJsonFragment(['success' => false]);
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_tasks_store_authorized(): void
+    {
+        // Arrange
+        Sanctum::actingAs(User::factory()->create());
+        $task = [
+            'name' => 'Test task',
+            'description' => 'Test description'
+        ];
+
+        // Act
+        $response = $this->post(self::API_ROUTE, $task);
+
+        // Assert
+        $response->assertJsonFragment(['success' => true]);
+        $response->assertStatus(Response::HTTP_CREATED);
+        $this->assertDatabaseHas((new Task())->getTable(), $task);
+    }
+
+    public function test_tasks_show_nonexistant_task(): void
+    {
+        // Arrange
+        Sanctum::actingAs(User::factory()->create());
+        $taskId = -1;
+
+        // Act
+        $response = $this->get(self::API_ROUTE . $taskId);
+
+        // Assert
+        $response->assertJsonFragment(['success' => false]);
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+    }
+
+    // to be continued
 }
